dc24c1447e04c6473f7911280908b30fd26e66dd
TalRasha <524948250@qq.com>
2025-02-07 14:22:48 +0800

    adjust folder

--- src/main/java/com/horadrim/khamlims/heart/KhalimsHeartApplication.java
+++ src/main/java/com/horadrim/khamlims/heart/KhalimsHeartApplication.java
@@ -1,4 +1,4 @@
-package com.horadrim.heart;
+package com.horadrim.khalims.heart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/horadrim/khamlims/heart/KhalimsHeartApplicationTests.java
+++ src/test/java/com/horadrim/khamlims/heart/KhalimsHeartApplicationTests.java
@@ -1,4 +1,4 @@
-package com.horadrim.heart;
+package com.horadrim.khalims.heart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
