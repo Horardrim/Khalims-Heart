a69759d98ea743e783e2bdc95071a283f3c8a011
TalRasha <524948250@qq.com>
2025-02-10 16:03:15 +0800

    integrate log4j

--- src/test/java/com/horadrim/khalims/heart/KhalimsHeartApplicationTests.java
+++ src/test/java/com/horadrim/khalims/heart/KhalimsHeartApplicationTests.java
@@ -3,11 +3,15 @@ package com.horadrim.khalims.heart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class KhalimsHeartApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Test log");
 	}
 
 }
